bbc9205457d3823eefd2a579dedd99d814ccf3f9
Pol <pol.querol@devinet.es>
2017-01-15 21:26:08 +0100

    First commit

---
+++ app/src/main/java/mdpa/lasalle/propertycross/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package mdpa.lasalle.propertycross.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mdpa.lasalle.propertycross.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
